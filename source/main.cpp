4729edc9a38e53fd2656571aec609cc131c25dab
circumscriptor <circumscriptor@protonmail.com>
2025-11-28 15:57:13 +0100

    initial commit

---
+++ source/main.cpp
@@ -0,0 +1,11 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_gpu.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_video.h>
+
+auto main(int argc, char** argv) -> int
+{
+    (void)argc;
+    (void)argv;
+    return 0;
+}
